320ef7c5d8eac1f0c4d3e5a27b02e29dd3e65e61
Gabriele-Viola <gabrieleviola50@gmail.com>
2025-03-18 11:31:01 +0100

    fix bootstrap modal

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -6,7 +6,7 @@
   
   <!-- Modal -->
   <div class="modal fade" id="videogameModal" tabindex="-1" aria-labelledby="videogameModalLabel" aria-hidden="true">
-    <div class="modal-dialog-centered">
+    <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
         <div class="modal-header">
           <h1 class="modal-title fs-5" id="videogameModalLabel">Are You sure to delete this videogame?</h1>
