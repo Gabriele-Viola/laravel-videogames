6dd7594644331382775a44b3cb84ce11c9321e27
Gabriele-Viola <gabrieleviola50@gmail.com>
2025-03-20 12:24:58 +0100

    add api route

---
+++ app/Http/Controllers/Api/VideogameController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Videogame;
+use Illuminate\Http\Request;
+
+class VideogameController extends Controller
+{
+    public function index()
+    {
+        $videogames = Videogame::with('genre')->get();
+
+        return response()->json([
+            'success' => true,
+            'data' => $videogames
+        ]);
+    }
+    public function show(Videogame $videogame)
+    {
+        $videogame->load('genre', 'platforms');
+        return response()->json([
+            'success' => true,
+            'data' => $videogame
+        ]);
+    }
+}
